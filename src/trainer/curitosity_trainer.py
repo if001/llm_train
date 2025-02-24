caf5ec394f2849926ec5e31071ff4df683c7beb1
if001 <otomijuf.004@gmail.com>
2025-02-24 23:59:01 +0900

    fix logging

--- src/trainer/curitosity_trainer.py
+++ src/trainer/curitosity_trainer.py
@@ -1,8 +1,11 @@
 from transformers import Trainer
+import wandb
 
 class CuriosityTrainer(Trainer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.custom_logs = {}
+
 
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
         input_ids = inputs["input_ids"]
@@ -15,14 +18,26 @@ class CuriosityTrainer(Trainer):
             labels=labels,
         )
         total_loss = outputs.loss + 0.1 * outputs.primary_loss + 0.05 * outputs.secondary_loss
+        
         ## callbackのlogsに記録
-        self.log({
-            "primary_loss": outputs.primary_loss.item() * 0.1,
-            "secondary_loss": outputs.secondary_loss.item() * 0.05,
-            "loss": outputs.loss.item(), 
-            "total_loss": total_loss.item()
+        self.custom_logs.update({
+            "train/primary_loss": outputs.primary_loss.item() * 0.1,
+            "train/secondary_loss": outputs.secondary_loss.item() * 0.05,
+            "train/total_loss": outputs.loss.item(),  # lm_loss
         })
 
         if return_outputs:
             return (total_loss, outputs)
         return total_loss
+
+    def log(self, logs: dict) -> None:
+        """
+        Trainerのlogをオーバーライドし、
+        Trainerのlogsと、custom_logsをまとめてwandbに送信
+        """
+        # stepなどをTrainerのlogsから取得し、custom_logに追加
+        self.custom_logs.update(logs)
+        if self.is_world_process_zero():
+            wandb.log(self.custom_logs, step=self.state.global_step)
+        self.custom_logs = {} # ログをクリア
+        super().log(logs) # Trainerのlogも行う    
